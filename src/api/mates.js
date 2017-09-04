7cb96a3989bbef578e102ad65dcbae60e71b2130
Sebastian Huber <fhs37229@fh-salzburg.ac.at>
2017-09-05 01:45:30 +0200

    feat: hook api

--- src/api/mates.js
+++ src/api/mates.js
@@ -1,43 +1,12 @@
-/**
- * Mocking client-server processing
- */
-const _mates = [
-  {
-    id: '550e8400-e29b-11d4-a716-446655440000',
-    image: 'https://lh3.googleusercontent.com/-HZ-Npth1HJ0/AAAAAAAAAAI/AAAAAAAABCc/80D_-U8XieQ/s120-p-rw-no/photo.jpg',
-    firstName: 'Sebastian',
-    lastName: 'Huber',
-    nickName: 'Seb',
-    isAway: true
-  },
-  {
-    id: '550e8400-e29b-11d4-a716-446655440001',
-    image: 'https://lh3.googleusercontent.com/-BWpKx4vfZvA/AAAAAAAAAAI/AAAAAAAAGB0/IArwNNQhpec/s120-p-rw-no/photo.jpg',
-    firstName: 'Daniel',
-    lastName: 'Trojer',
-    nickName: 'Danilo',
-    isAway: false
-  },
-  {
-    id: '550e8400-e29b-11d4-a716-446655440002',
-    image: 'https://lh3.googleusercontent.com/-QsOaqulZNh8/AAAAAAAAAAI/AAAAAAAAAAA/APJypA3aaIVGNTeYPd5dMP2TFYG-7HmrEQ/s120-p-rw-no-mo/photo.jpg',
-    firstName: 'Alexander',
-    lastName: 'Moser',
-    nickName: 'Alex',
-    isAway: false
-  },
-  {
-    id: '550e8400-e29b-11d4-a716-446655440003',
-    image: 'https://lh3.googleusercontent.com/-ApByI-bY9gc/AAAAAAAAAAI/AAAAAAAACws/qzjbwODH8Zs/s120-p-rw-no/photo.jpg',
-    firstName: 'Josef',
-    lastName: 'Krabath',
-    nickName: 'Sepp',
-    isAway: true
-  }
-]
+import http from './../resource'
 
 export default {
-  getMates (cb) {
-    setTimeout(() => cb(_mates), 100)
+  async getMates (callback) {
+    try {
+      const response = await http.get('users')
+      callback(response.data)
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,14 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './stores'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+export default new Vue({
   el: '#app',
   store,
   router,
+  http: {
+    root: 'http://192.168.0.21:3000'
+  },
   template: '<App/>',
   components: { App }
 })
